cacccd9e6cb2afa83dfaba692b5dc06205e17a0d
Rodrigo Dominguez <rodrigo.dominguez@alumnos.ucn.cl>
2021-05-01 20:29:02 -0400

    user routes login, logout

--- Backend/apirest/routes/api.php
+++ Backend/apirest/routes/api.php
@@ -17,12 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'auth'
 ], function () {
+    //rutas publicas
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signUp');
 
     Route::group([
         'middleware' => 'auth:api'
     ], function () {
+        //rutas con autenticacion
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
     });
